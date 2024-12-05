f7e162a616b69ceced1c46ab9c6946608a5e24b0
Nikola <nikola.brajkovic88@gmail.com>
2024-12-05 10:31:34 +0100

    Access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,21 @@
-# first push
+
+import json
+
+
+def load_data(file_path):
+    with open(file_path, "r") as file:
+        return json.load(file)
+
+animals_data = load_data("animals_data.json")
+
+for animal in animals_data:
+    animal_name = animal["name"]
+    animal_diet = animal["characteristics"]["diet"]
+    animal_location = animal["locations"][0]
+    animal_type = animal["characteristics"].get("type", "Unknown")
+    print(
+        f"Name: {animal_name}\n"
+        f"Diet: {animal_diet}\n"
+        f"Location: {animal_location}\n"
+        f"Type: {animal_type}\n"
+    )
